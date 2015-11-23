d3c410256dfaadf91b714749bcd8dc4e1d2dbd4b
Wilson Rondini <wilbecoding@gmail.com>
2015-11-22 21:14:45 -0700

    sidebar fully functional

---
+++ public/javascripts/jquery.js
@@ -0,0 +1,169 @@
+$( document ).ready(function() {
+
+  $('.hailiangButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+        $('.blackwaterMidstream').addClass('hide'); 
+        $('.codeRebel').addClass('hide'); 
+        $('.cyberCity').addClass('hide'); 
+        $('.flikMedia').addClass('hide'); 
+        $('.forgiven').addClass('hide'); 
+        $('.fotoFwd').addClass('hide'); 
+        $('.heartTestLabs').addClass('hide'); 
+        $('.moorSkincare').addClass('hide'); 
+        $('.myClubBetting').addClass('hide'); 
+        $('.sportsTradex').addClass('hide'); 
+        $('.hailiangEducationGroup').removeClass('hide')
+  })
+
+  $('.blackwaterButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.blackwaterMidstream').removeClass('hide')
+    console.log("blackwater hits");
+  })
+
+  $('.codeRebelButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.codeRebel').removeClass('hide')
+  })
+
+  $('.cyberCityButton').on('click',function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.cyberCity').removeClass('hide')
+  })
+
+  $('.flikMediaButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.flikMedia').removeClass('hide')
+  })
+
+  $('.forgivenButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.forgiven').removeClass('hide')
+  })
+
+  $('.fotoFwdButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.fotoFwd').removeClass('hide')
+  })
+
+  $('.heartTestLabsButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.heartTestLabs').removeClass('hide')
+  })
+
+  $('.moorSkincareButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.moorSkincare').removeClass('hide')
+  })
+
+  $('.myClubBettingButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.myClubBetting').removeClass('hide')
+  })
+
+  $('.sportsTradexButton').on('click', function(){
+    $('.hailiangEducationGroup').addClass('hide');
+    $('.blackwaterMidstream').addClass('hide'); 
+    $('.codeRebel').addClass('hide'); 
+    $('.cyberCity').addClass('hide'); 
+    $('.flikMedia').addClass('hide'); 
+    $('.forgiven').addClass('hide'); 
+    $('.fotoFwd').addClass('hide'); 
+    $('.heartTestLabs').addClass('hide'); 
+    $('.moorSkincare').addClass('hide'); 
+    $('.myClubBetting').addClass('hide'); 
+    $('.sportsTradex').addClass('hide'); 
+    $('.sportsTradex').removeClass('hide')
+  })
+
+});
